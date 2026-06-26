3fe78cdb28ad1aa06997bd99cc8972a9b240e721
Luis Herrera <luis1818.lh@gmail.com>
2026-06-25 23:16:41 -0500

    fix: show active ram in tmux

--- tmux/scripts/ram.sh
+++ tmux/scripts/ram.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env sh
 
+page_size=$(vm_stat | awk '/page size of/ { gsub(/\./, "", $8); print $8 }')
 total_gib=$(sysctl -n hw.memsize | awk '{ printf "%.0f", $1 / 1024 / 1024 / 1024 }')
-used=$(top -l 1 -n 0 | awk '/PhysMem:/ { print $2 }')
 
-if [ -n "$used" ] && [ -n "$total_gib" ]; then
-  printf 'RAM %s/%sGiB' "$used" "$total_gib"
+used_gib=$(
+  vm_stat | awk -v page_size="$page_size" '
+    /Pages active/ { gsub(/\./, "", $3); active = $3 }
+    /Pages wired down/ { gsub(/\./, "", $4); wired = $4 }
+    /Pages occupied by compressor/ { gsub(/\./, "", $5); compressed = $5 }
+    END {
+      used_bytes = (active + wired + compressed) * page_size
+      printf "%.0f", used_bytes / 1024 / 1024 / 1024
+    }
+  '
+)
+
+if [ -n "$used_gib" ] && [ -n "$total_gib" ]; then
+  printf 'RAM %s/%sGiB' "$used_gib" "$total_gib"
 fi
